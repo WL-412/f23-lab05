93b7750d010fb80eae2460f7f4328c452e276862
Wenhan Li <wenhan@wenhans-air.wifi.local.cmu.edu>
2023-11-26 18:25:52 -0500

    inheritance and delegation

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,50 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private SortedIntList sortedIntList = new SortedIntList();
+    private int totalAdded = 0;
 
+    @Override
+    public boolean add(int value) {
+        if (sortedIntList.add(value)) {
+            totalAdded ++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        if (sortedIntList.addAll(list)) {
+            totalAdded = size();
+            return true;
+        }
+        return false;
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    @Override
+    public int get(int index) {
+        return sortedIntList.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return sortedIntList.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedIntList.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return sortedIntList.size();
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,35 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+ 
 
+public class InheritanceSortedIntList extends SortedIntList{
+    // Write your implementation below with API documentation
+    private int totalAdded;
+
+    public InheritanceSortedIntList() {
+        totalAdded = 0;
+    }
+
+    @Override
+    public boolean add(int value) {
+        if (super.add(value)) {
+            totalAdded ++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override 
+    public boolean addAll(IntegerList list) {
+        if (super.addAll(list)) {
+            totalAdded = size();
+            return true;
+        }
+        return false;
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
